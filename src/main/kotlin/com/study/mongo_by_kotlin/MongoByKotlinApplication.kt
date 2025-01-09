553ae5e9a2b65c066f6bc5fb28ea17b607e28144
zephyr7419 <zephyr@musma.net>
2025-01-09 14:49:46 +0900

    init: init project

---
+++ src/main/kotlin/com/study/mongo_by_kotlin/MongoByKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.study.mongo_by_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongoByKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MongoByKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/mongo_by_kotlin/MongoByKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.mongo_by_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoByKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
